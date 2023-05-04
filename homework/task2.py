7b4b6b90412e7e7537ad33f2b8be8ac7a0368a25
Парадеева Н.М <nm.paradeeva@region.tensor.ru>
2023-05-04 15:21:53 +0500

    new

---
+++ homework/task2.py
@@ -0,0 +1,13 @@
+side_of_square = float(2)  # задача 1
+perimeter_of_square = side_of_square * 4
+square_area_of_square = side_of_square ** 2
+diagonal_of_square = ((side_of_square ** 2) * 2) ** 0.5
+print(perimeter_of_square, square_area_of_square, diagonal_of_square)
+
+text1 = 'Наступила весна.'
+text2 = 'Cтало тепло.'
+text1.replace(text1х)
+text3 = text1 + ' ' + text2
+print(text3)
+print(text3.replace(text3[0:1], text3[17:18]))
+
